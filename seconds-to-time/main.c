1229a691bacb1f83c91715035366e14628351380
“Vladyslav <vladyslav.borozna@isisbem.it>
2019-10-31 18:41:25 +0100

    Transform seconds in hours, minutes and seconds.

--- seconds-to-time/main.c
+++ seconds-to-time/main.c
@@ -8,8 +8,23 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+void split_time(long int tot_sec, int *h, int *m, int *s);
+
+int main() {
+    int h, m, s;
+    long tot_sec = 5712;
+    
+    split_time(tot_sec, &h, &m, &s);
+    printf("The time is: %dh %dm %ds\n", h, m, s);
 }
+
+void split_time(long int tot_sec, int *h, int *m, int *s) {
+    
+    float tempo;
+    tempo = tot_sec / 3600;//transform in hours
+    *h = tempo;
+    *m = (tot_sec % 3600) / 60;//transform in minutes
+    *s = (tot_sec % 3600) % 60;//transform in seconds
+}
+
+
